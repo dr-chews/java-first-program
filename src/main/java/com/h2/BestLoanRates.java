66a9330f28f0bf5cbc84003c78c67aedd56cbd80
drchews <jesus.w.hdz@gmail.com>
2021-02-27 23:58:33 -0600

    Completed module

--- src/main/java/com/h2/BestLoanRates.java
+++ src/main/java/com/h2/BestLoanRates.java
@@ -4,7 +4,7 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class BestLoanRates {
-
+    // checking
     public static final Map<Integer, Float> bestRates = Map.of(1, 5.50f, 2, 3.45f, 3, 2.67f);
 
     public static void main(String[] args){
